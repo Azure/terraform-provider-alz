b57e35a361070906ce393300bfcebc27ab0e3f26
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-07-04 12:17:04 +0100

    chore: update some stuff in root

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 
 	opts := providerserver.ServeOpts{
 		// TODO: Update this string with the published name of your provider.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Address: "registry.terraform.io/Azure/alz",
 		Debug:   debug,
 	}
 
