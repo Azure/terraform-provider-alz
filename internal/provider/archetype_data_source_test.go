e73e9d666e7c3294f12c7a50c8a716d0546c760c
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-10-06 15:13:56 +0100

    test: update test for new alzlib

--- internal/provider/archetype_data_source_test.go
+++ internal/provider/archetype_data_source_test.go
@@ -58,8 +58,8 @@ func TestAccAlzArchetypeDataSourceFullAlz(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.alz_archetype.root", "id", "root"),
 					resource.TestCheckResourceAttr("data.alz_archetype.root", "alz_policy_assignments.%", "13"),
-					resource.TestCheckResourceAttr("data.alz_archetype.root", "alz_policy_definitions.%", "124"),
-					resource.TestCheckResourceAttr("data.alz_archetype.root", "alz_policy_set_definitions.%", "12"),
+					resource.TestCheckResourceAttr("data.alz_archetype.root", "alz_policy_definitions.%", "126"),
+					resource.TestCheckResourceAttr("data.alz_archetype.root", "alz_policy_set_definitions.%", "13"),
 					resource.TestCheckResourceAttr("data.alz_archetype.root", "alz_role_definitions.%", "5"),
 				),
 			},
