121092a49eed4836a89e378ca056b076b58b44ed
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-03-06 20:48:08 +0000

    docs: minor corrections post v0.10.0 (#76)

--- examples/data-sources/alz_archetype/data-source.tf
+++ examples/data-sources/alz_archetype/data-source.tf
@@ -4,10 +4,8 @@ data "alz_archetype" "example" {
   defaults = {
     location = "westeurope"
   }
-  id                        = "alz-root"
-  base_archetype            = "root"
-  display_name              = "alz-root"
-  parent_id                 = data.azurerm_client_config.current.tenant_id
-  policy_definitions_to_add = ["MyPolicyDefinition"]
-  policy_assignments_to_add = ["MyPolicyAssignment"]
+  id             = "alz-root"
+  base_archetype = "root"
+  display_name   = "alz-root"
+  parent_id      = data.azurerm_client_config.current.tenant_id
 }
--- examples/data-sources/alz_archetype_keys/data-source.tf
+++ examples/data-sources/alz_archetype_keys/data-source.tf
@@ -1,5 +1,3 @@
 data "alz_archetype_keys" "example" {
-  base_archetype            = "root"
-  policy_definitions_to_add = ["MyPolicyDefinition"]
-  policy_assignments_to_add = ["MyPolicyAssignment"]
+  base_archetype = "root"
 }
