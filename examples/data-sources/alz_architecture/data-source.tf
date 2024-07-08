755887fd58046f08b03906e0d996db2913916e85
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-07-08 16:25:11 +0100

    feat: alz2024-07-02 (#118)

--- examples/data-sources/alz_architecture/data-source.tf
+++ examples/data-sources/alz_architecture/data-source.tf
@@ -3,5 +3,10 @@ data "azurerm_client_config" "example" {}
 data "alz_architecture" "example" {
   name                     = "alz"
   root_management_group_id = azurerm_client_config.example.tenant_id
-  location                 = "westus2"
+  location                 = "northeurope"
+}
+
+output "managment_groups" {
+  description = "The management groups of the architecture as list of objects."
+  value       = data.alz_architecture.example.managment_groups
 }
